d40d1e9751b3dc7db7a5227b0af4f7d63875dde3
Karlen Shahinyan <shkarlen@gmail.com>
2019-10-08 18:11:00 -0700

    trying to find duplicate requests

--- 311Data/ETL/utils.py
+++ 311Data/ETL/utils.py
@@ -1,10 +1,15 @@
 import numpy as np
 import pandas as pd
 
-from shapely import wkt, Point
+from shapely import wkt
 
 def fill_placeholder_1900(df):
     return df.replace(to_replace=pd.to_datetime('1900'),value=pd.NaT)
+
+def to_datetime(df):
+    dt_cols = ['CreatedDate','UpdatedDate','ServiceDate','ClosedDate']
+    for col in dt_cols:
+        df[col] = pd.to_datetime(df[col])
 
 def fill_placeholder_1900_col(df):
     dt_cols = ['CreatedDate','UpdatedDate','ServiceDate','ClosedDate']
@@ -27,3 +32,15 @@ def to_points(p):
         return p
     else:
         return wkt.loads('Point{}'.format(p.replace(',',' ')))
+    
+    
+def to_geom(df):
+    df['Location'] = df.Location.apply(to_points)
+
+def add_datediff_cols(df):
+    df['ClosedDiff'] = df.ClosedDate - df.CreatedDate
+    df['ServiceDiff'] = df.ServiceDate - df.CreatedDate
+    df['ClosedServiceDiff'] = df.ClosedDate - df.ServiceDate
+    df['ClosedDiff_Days'] = df.ClosedDiff.apply(ddiff2days)
+    df['ServiceDiff_Days'] = df.ServiceDiff.apply(ddiff2days)
+    df['ClosedServiceDiff_Days'] = df.ClosedServiceDiff.apply(ddiff2days)

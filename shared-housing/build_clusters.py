6f3712d13a4e2cc0d0cb81963d3436858844e660
Karlen Shahinyan <shkarlen@gmail.com>
2019-12-03 22:34:15 -0800

    added functionality for choosing best-match groups

--- shared-housing/build_clusters.py
+++ shared-housing/build_clusters.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import List
+from itertools import combinations
 
 class Person:
 
@@ -12,7 +13,17 @@ class Person:
         score = 0
 
         for response in self.responses:
-            score += other.responses[response.conflict]
+
+            try:
+                resp_match = other.responses[response['id']]
+            except KeyError:
+                continue
+
+            for val in response['value_not_in']:
+                if val in resp_match['value']:
+                    #can store this as a dict or something to have
+                    #more detailed matching info
+                    sore += response['weight']
 
         return score
 
@@ -31,12 +42,58 @@ class Cluster:
 
         conflict_score = 0
         for other in self.person_list:
+            # probably just replace this with the matrix
             conflict_score += person.pair_with_other(other)
 
         return conflict_score
 
 
-def build_conflict_matrix(person_list: List):
+def build_conflict_matrix(person_list: List[Person]) -> np.matrix:
 
     n_people = len(person_list)
-    conflict_matrix = np.zeros([n_people,n_people])
+    conflict_matrix = np.full([n_people,n_people],np.inf)
+
+    for i in range(n_people):
+        for j in range(n_people):
+            if i == j:
+                conflict_matrix[i,j] = 0
+            else:
+                conlict_matrix[i,j] = person_list[i].pair_with_other(
+                    person_list[j]
+                )
+
+    return conflict_matrix
+
+
+def find_best_cluster(conflict_matrix: np.matrix,
+                      n_people: int, n_iter: int) -> List[Person]:
+
+    best_score = 0
+    best_group = []
+    #tried = []
+
+    combs = combinations(range(conflict_matrix.shape[0])), n_people)
+    #can also loop through all of these...
+
+    #for _ in range(n_iter):
+    for test_people in combs:
+
+        score = 0
+
+        #test_people = random.sample(range(0, conflict_matrix.shape[0]),
+        #                            n_people)
+
+        #while set(test_people) in tried:
+        #    test_people = random.sample(range(0, conflict_matrix.shape[0]),
+        #                                n_people)
+        #tried.append(set(test_people))
+
+        for i in range(n_people):
+            for j in range(i+1,n_people):
+                score += conflict_matrix(i,j)
+
+        if score < best_score:
+            best_score = score
+            best_group = test_people
+
+    return best_group, best_score
